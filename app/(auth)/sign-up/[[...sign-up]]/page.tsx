1472a41b80a067471350df7eede1cdd562334645
Alok Verma <alokverma73762@gmail.com>
2024-04-06 03:30:54 +0530

    Initail Commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
+const SignUpPage = () => {
+  return (
+    <main className='flex-center h-screen w-full'>
+        <SignUp />
+    </main>
+  )
+}
+
+export default SignUpPage
